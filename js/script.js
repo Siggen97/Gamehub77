957c8136021676290c3ca6087ccb8192b72012c5
Siggen97 <104718385+Siggen97@users.noreply.github.com>
2023-04-20 17:37:30 +0200

    added more

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// All Entries
+// Get https://api.noroff.dev/api/v1//gamehub
+// This endpoint allows you to retrive all products
+
+[
+    {
+      "id": "ded6041a-622f-4fb4-81e4-96fcfdad4dff",
+      "title": "Ping Pong Championship",
+      "description": "Enter the world of Ping Pong Championship and compete against the world's best to become the ultimate champion in this exciting game.",
+      "genre": "Sports",
+      "released": "2005",
+      "ageRating": "3+",
+      "price": 14.99,
+      "discountedPrice": 4.79,
+      "onSale": true,
+      "image": "https://api.noroff.dev/images/gamehub/0-ping-pong-championship.jpg",
+      "tags": ["gamehub", "game"],
+      "favorite": true
+    },
+    {
+      "id": "2ace4e1d-cad7-4d35-8d59-6c9ac3e3eaf8",
+      "title": "Super Duper",
+      "description": "Celebrate some of the world's supe duper Superheroes with augmented reality.",
+      "genre": "Adventure",
+      "released": "2006",
+      "ageRating": "3+",
+      "price": 15.99,
+      "discountedPrice": 15.99,
+      "onSale": false,
+      "image": "https://api.noroff.dev/images/gamehub/1-super-duper.jpg",
+      "tags": ["gamehub", "game"],
+      "favorite": true
+    },
+    // ...
+    
+    // Single Entries
+    // Get https://api.noroff.dev/api/v1//gamehub/ <id>
+    // This endpoint allows you to retrive a single product by its id
+
+    {
+        "id": "ded6041a-622f-4fb4-81e4-96fcfdad4dff",
+        "title": "Ping Pong Championship",
+        "description": "Enter the world of Ping Pong Championship and compete against the world's best to become the ultimate champion in this exciting game.",
+        "genre": "Sports",
+        "released": "2005",
+        "ageRating": "3+",
+        "price": 14.99,
+        "discountedPrice": 4.79,
+        "onSale": true,
+        "image": "https://api.noroff.dev/images/gamehub/0-ping-pong-championship.jpg",
+        "tags": ["gamehub", "game"],
+        "favorite": true
+    }
+]
