ad63ae4801e3399e29675f6855150420ebc274d8
Siggen97 <104718385+Siggen97@users.noreply.github.com>
2023-05-01 09:54:27 +0200

    added javascript to the menuBar button, used show_hide function

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ const loader = document.getElementById('preLoader');
 window.addEventListener('load', function () {
 	loader.style.display = 'none';
 });
+
+var a;
+function show_hide () {
+	if (a==1){
+		document.getElementsByTagName('nav')[0].style.display = 'flex';
+		return a=0
+	}
+	else {
+		document.getElementsByTagName('nav')[0].style.display = 'none';
+		return a=1
+	}
+}
+
