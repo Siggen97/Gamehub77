be92be40a63de5082c5198307411214ea13d3382
Siggen97 <104718385+Siggen97@users.noreply.github.com>
2023-06-04 11:30:21 +0200

    Change JS
    Made the gamepage look a bit more attractive, bigger img, better cart button, and made the info display on the side of the page

--- js/game-page.js
+++ js/game-page.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const params = new URLSearchParams(window.location.search);
 const productId = params.get('id');
 
@@ -7,41 +9,45 @@ fetch(apiUrl)
 	.then((response) => response.json())
 	.then((data) => {
 		const outerDiv = document.createElement('div');
-		const game2Div = document.createElement('div');
-		game2Div.classList.add('XBOXgame');
-		outerDiv.appendChild(game2Div);
+		outerDiv.classList.add('product-container');
 
-		const productTitle = document.createElement('h3');
-		productTitle.innerText = data.title;
-		outerDiv.appendChild(productTitle);
-
+		const gameImageDiv = document.createElement('div');
+		gameImageDiv.classList.add('game-image');
 		const productImage = document.createElement('img');
 		productImage.src = data.image;
-		game2Div.appendChild(productImage);
-
-		const productPrice = document.createElement('p');
-		productPrice.innerText = 'Price: ' + data.price;
-		game2Div.appendChild(productPrice);
-
-		const releaseDate = document.createElement('p');
-		releaseDate.innerText = 'Release Date: ' + data.released;
-		game2Div.appendChild(releaseDate);
-
-		const ageRating = document.createElement('p');
-		ageRating.innerText = data.ageRating;
-		game2Div.appendChild(ageRating);
-
+		gameImageDiv.appendChild(productImage);
+		outerDiv.appendChild(gameImageDiv);
+		
 		const titleDescription = document.createElement('div');
-		titleDescription.classList.add('titleDescription');
-
+		titleDescription.classList.add('title-description');
+		
+		const productInfoDiv = document.createElement('div');
+		productInfoDiv.classList.add('product-info');
+		
+		const productTitle = document.createElement('h3');
+		productTitle.innerText = data.title;
+		productInfoDiv.appendChild(productTitle);
+		
 		const description = document.createElement('p');
-		description.classList.add('gameDescription');
+		description.classList.add('game-description');
 		description.innerText = data.description;
 		titleDescription.appendChild(productTitle);
 		titleDescription.appendChild(description);
-		outerDiv.appendChild(titleDescription);
+		productInfoDiv.appendChild(titleDescription);
+
+		const productPrice = document.createElement('p');
+		productPrice.innerText = 'Price: ' + data.price;
+		productInfoDiv.appendChild(productPrice);
+		
+		const releaseDate = document.createElement('p');
+		releaseDate.innerText = 'Release Date: ' + data.released;
+		productInfoDiv.appendChild(releaseDate);
+		
+		const ageRating = document.createElement('p');
+		ageRating.innerText = data.ageRating;
+		productInfoDiv.appendChild(ageRating);
+		
+		outerDiv.appendChild(productInfoDiv);
 
 		document.querySelector('section').appendChild(outerDiv);
-	})
-
-	
+	});
